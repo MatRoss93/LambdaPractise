f8880ddb1f720679ef87645b1fee9637a4faf9db
MatRoss93 <rozen1036@gmail.com>
2019-03-21 10:22:12 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,17 @@ public class Main {
 
     public static void lambdaTest(String s1, String s2, String s3, StringProcessing strings) {
 
-        System.out.println(strings.go(s1, s2, s3));
+        //System.out.println(strings.go(s1, s2, s3));
     }
     public static void main(String... args) {
 
-        String s = "aaaaaa";
+        
       
 
         int add = 10 + 20;
         int sum = 0;
        
         System.out.println(sum);
-        sum += add;
+        
     }
 }
