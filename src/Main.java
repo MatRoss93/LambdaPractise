e518d1a96b4fe5b10f0840c11b021611603efd8f
MatRoss93 <rozen1036@gmail.com>
2019-03-21 10:09:53 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,6 @@ public class Main {
     public static void main(String... args) {
 
         String s1 = "lambda";
-        String s2 = "przetwarzanie";
-        String s3 = "nauka";
-        String s4 = "do git commit problem";
+        
     }
 }
