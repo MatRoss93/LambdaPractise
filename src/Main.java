6f33006ab1391f13b6f176538aaaca0e9cb230be
MatRoss93 <rozen1036@gmail.com>
2019-03-21 10:09:40 +0100

    Interfesj

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
 
-    public static String lambdaTest(String s1, String s2, String s3, StringProcessing strings) {
+    public static void lambdaTest(String s1, String s2, String s3, StringProcessing strings) {
 
-        return "";
+        System.out.println(strings.go(s1, s2, s3));
     }
     public static void main(String... args) {
 
         String s1 = "lambda";
         String s2 = "przetwarzanie";
-
+        String s3 = "nauka";
+        String s4 = "do git commit problem";
     }
 }
