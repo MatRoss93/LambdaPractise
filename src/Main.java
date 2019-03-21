73686a321c82c9b9ce59136277513c701a2851db
MatRoss93 <rozen1036@gmail.com>
2019-03-21 10:18:02 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,7 @@ public class Main {
     }
     public static void main(String... args) {
 
-        String s1 = "lambda";
-        String s2 = "przetwarzanie";
-        String s3 = "nauka";
-        String s4 = "do git commit problem";
+      
 
         int add = 10 + 20;
         int sum = 0;
