f0fd7fe96a954fc782932633c82e295cd813beb9
MatRoss93 <rozen1036@gmail.com>
2019-03-21 10:43:16 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,5 @@ public class Main {
     public static void main(String... args) {
 
         
-      
-
-        int add = 10 + 20;
-        int sum = 0;
-        for(int i = 0; i < 10; i ++) {
-            sum += i;
-        }
-        System.out.println(sum);
-        
     }
 }
